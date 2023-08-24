4922cfd5c4221a53921d08229b8e70f83fe2f784
Theamanthenua <132770319+Theamanthenua@users.noreply.github.com>
2023-08-24 12:06:46 +0530

    Add files via upload

---
+++ mean_var_std.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+def calculate(numbers):
+    if len(numbers) != 9:
+        raise ValueError("List must contain nine numbers.")
+
+    matrix = np.array(numbers).reshape(3, 3)
+
+    result = {
+        'mean': [list(matrix.mean(axis=1)), list(matrix.mean(axis=0)), matrix.mean()],
+        'variance': [list(matrix.var(axis=1)), list(matrix.var(axis=0)), matrix.var()],
+        'standard deviation': [list(matrix.std(axis=1)), list(matrix.std(axis=0)), matrix.std()],
+        'max': [list(matrix.max(axis=1)), list(matrix.max(axis=0)), matrix.max()],
+        'min': [list(matrix.min(axis=1)), list(matrix.min(axis=0)), matrix.min()],
+        'sum': [list(matrix.sum(axis=1)), list(matrix.sum(axis=0)), matrix.sum()]
+    }
+
+    return result
+
+from mean_var_std import calculate
+
+# Test your function with example input
+input_list = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+result = calculate(input_list)
+print(result)
